4056e948da3b24ffeff67c8920a8357e5ea68b10
Pablo <pnavarrete1985@gmail.com>
2024-12-15 17:21:38 -0300

    Corrijo main.tf con variables cargadas

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 terraform {
   required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.1"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
+  required_version = ">= 1.4.0"
 }
 
 # Declarar las variables

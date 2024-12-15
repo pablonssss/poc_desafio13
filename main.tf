499f1308d493ff55bc9ecbbe9e328dd715f18e27
Pablo <pnavarrete1985@gmail.com>
2024-12-15 15:36:35 -0300

    Agrego variables en main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,24 @@ terraform {
   }
 }
 
+# Declarar las variables
+variable "AWS_ACCESS_KEY_ID" {
+  description = "Clave de acceso de AWS"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "Clave secreta de AWS"
+  type        = string
+}
+
+# Configurar el proveedor de AWS usando las variables
+provider "aws" {
+  region                  = "us-east-1"
+  access_key              = var.AWS_ACCESS_KEY_ID
+  secret_access_key       = var.AWS_SECRET_ACCESS_KEY
+}
+
 provider "null" {}
 
 resource "null_resource" "dummy" {
